7c8c379f5773271fae9d9b7106903967909d1f1d
Andrea <andreayoung5350@gmail.com>
2024-12-11 11:26:22 -0500

    added SessionOptions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import UserRoutes from './Users/routes.js';
 import ProductRoutes from './Products/routes.js';
 import ReveiwRoutes from './Reviews/routes.js';
+import session from "express-session";
 
 const CONNECTION_STRING = "mongodb://localhost:27017/swatched"
 mongoose.connect(CONNECTION_STRING);
@@ -12,7 +13,21 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "swatched",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
 
+app.use(session(sessionOptions));
 app.use(express.json()); //endure this statement is after CORS call
 //approach to encode the data as JSON in the HTTP request body which allows for arbitrarily large amounts of data as well as secure data encryption.
 
