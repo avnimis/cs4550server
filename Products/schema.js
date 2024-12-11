af08c815d34c4236c49536d9460b4c268f2c8154
Andrea <andreayoung5350@gmail.com>
2024-12-10 23:26:01 -0500

    products schema

--- Products/schema.js
+++ Products/schema.js
@@ -2,27 +2,28 @@ import mongoose from "mongoose"; //load mongoose library
 
 const productSchema = new mongoose.Schema({ //create schema
 
-    //String field that is required and unique
-    name: { 
-      type: String, 
-      required: true, 
-      unique: true,
-      trim: true },
-    //string field that is required, but not unique
-    description: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    image: {
-      type: String,
-      required: true // URL to the product image
-    },
-    tags: {
-      type: [String], // Array of tags (e.g., ["Vegan", "Cruelty-Free"])
-      default: []
-    },
+  //String field that is required and unique
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
   },
+  //string field that is required, but not unique
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    required: true // URL to the product image
+  },
+  tags: {
+    type: [String], // Array of tags (e.g., ["Vegan", "Cruelty-Free"])
+    default: []
+  },
+},
   { collection: "products" } // store data in "products" collection
 );
 
